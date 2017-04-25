6ec37671bacc0f108408a1cd86dcedc0fe858b45
Tosh Camille <tochecamille@gmail.com>
2017-04-25 15:12:13 +0200

    get All servers

--- app/api.py
+++ app/api.py
@@ -11,6 +11,13 @@ class API(object):
 		self.addr = j['api']
 
 
+	def loadServers(self):
+		r = requests.post(self.addr + "listvm", data="null")
+		req = json.loads(r.text)
+
+		return req
+
+
 	def loadServer(self, name):
 		r = requests.post(self.addr + "getvm", data=name)
 		req = json.loads(r.text)
